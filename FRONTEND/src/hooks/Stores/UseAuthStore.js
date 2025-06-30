6ae0ada3d0a6d3d37dd385a2cfcd8bc4fc6c34be
Maherisoa Nosilala <nosilala13@gmail.com>
2025-06-30 21:48:24 +0300

    Mettre en place le service pour la transcription

--- FRONTEND/src/hooks/Stores/UseAuthStore.js
+++
@@ -1,9 +0,0 @@
-import create from 'zustand';
-
-export const useAuthStore = create((set) => ({
-    transcripions: [],
-    currentTranscription: null,
-    loading:false,
-    error:null,
-    setTranscriptions: (transcripions) => set({ transcripions }),
-}));
