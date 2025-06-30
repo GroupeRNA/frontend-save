cf87f1744f196c86b972fd0724c93d043f15719a
Maherisoa Nosilala <nosilala13@gmail.com>
2025-06-30 17:11:57 +0300

    Fusion des historiques divergents

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Test from "./components/Test/Test"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='text-3xl font-bold  text-blue-700 m-10'>
-        Hello , welcome to RNA project. 
-      </h1>
-    </>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Test />} />
+  </Routes>
+  </BrowserRouter>
   )
 }
 
