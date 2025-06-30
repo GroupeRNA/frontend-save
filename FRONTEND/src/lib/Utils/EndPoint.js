cda8658dae2e615ddb9368fd239dbdfa1347b03e
Maherisoa Nosilala <nosilala13@gmail.com>
2025-06-30 21:11:07 +0300

    Configuration de l'API et les endpoints

---
+++ FRONTEND/src/lib/Utils/EndPoint.js
@@ -0,0 +1,7 @@
+//Configuration de l'endpoint pour les requêtes HTTP
+export const EndPoint = {
+    CREATE_AUDIO: "/create_audio",
+    CREATE_TRANSCRIPTION: "/create_transcription",
+    DELETE_AUDIO: "/delete_audio/:audio_id",
+    DELETE_TRANSCRIPTION: "/delete_transcription/:transcription_id",
+}
