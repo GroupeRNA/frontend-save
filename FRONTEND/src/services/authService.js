8e14760fc57650515bee77b8972c084b892b4f21
tovogilbert <tovogilbert283@gmail.com>
2025-06-30 21:33:01 +0300

    Correction user service

--- FRONTEND/src/services/authService.js
+++ FRONTEND/src/services/authService.js
@@ -1,9 +1,9 @@
-import api from "../data/api/axios";
-import { AUTH } from "../data/constants/endPoint";
+import api from "../data/api/api";
+import { REGISTER ,LOGIN } from "../data/constants/endpoints";
 
 class AuthService {
   static async register(userData) {
-    const response = await api.post(AUTH.REGISTER, {
+    const response = await api.post(REGISTER, {
       "user_name": userData.user_name,
       "user_mail": userData.user_mail,
       "user_password": userData.user_password,
@@ -12,7 +12,7 @@ class AuthService {
   }
 
   static async login(userData) {
-    const response = await api.post(AUTH.LOGIN, {
+    const response = await api.post(LOGIN, {
       "user_name": userData.user_name,
       "user_password": userData.user_password,
     });
